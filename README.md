6d4ea258ccfa21bd85eacd787417880f83b4a432
zc12345 <18292885866.com>
2018-01-22 03:25:49 +0800

    can be interrupted and continue

--- README.md
+++ README.md
@@ -2,9 +2,21 @@
 
 ## 1. Matlab
 
-1. Read data from images and annotation xml files;
-2. Reorganize data structure;
-3. Save data into .mat files.
+#### 1-1 Main idea
+1. Annotate selected images (more details at 2-1);
+2. Read data from images and annotation xml files;
+3. Reorganize data structure;
+4. Save data into .mat files.
+
+#### 1-2 Usage
+```
+$ matlab
+> cd matlab
+> run img_anno.m
+> run getData.m
+> run checkData.m
+> run splitData.m
+```
 
 ## 2. Python
 
@@ -14,17 +26,19 @@
 3. Create same-name annotation xml file according to example xml annotation;
 4. Call GUI & annotate using mouse click.
 
-#### 2-2 file directory structure
+#### 2-2 File directory structure
 
 ```
 ./--data/----annotation/
   |       |--image/ #target dir
+  |       |--validation/---annotation/ #validation dir
+  |       |              `-image/
   |       `--rar/   #source dir
   |-python/--anno_img.py
   `-matlab/
 ```
 
-#### 2-3 usage
+#### 2-3 Usage
 organize data as above and run python script.
 ```
 $ cd python/
--- python/anno_img.py
+++ python/anno_img.py
@@ -3,20 +3,21 @@
 from __future__ import print_function
 from xml.dom.minidom import Document
 from os import listdir
-from os.path import isfile, join, splitext
+from os.path import exists,isfile, join, splitext
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-def select_pic(source_dir, target_dir, step = 20):
+def select_pic(source_dir, target_dir, step = 20, index = 1):
     '''
-    每隔step张图片选取一张，并复制到target_dir文件夹，默认步长为20
+    每隔step张图片选取一张，并复制到target_dir文件夹，默认步长为20；
+    并根据原文件夹进行重命名
     '''
     files = [f for f in listdir(source_dir) if isfile(join(source_dir,f))]
     n = len(files)
     for i in range(n):
         source_file = join(source_dir, files[i])
-        target_file = join(target_dir, files[i])
+        target_file = join(target_dir, 'sec'+str(index)+files[i])#对原文件根据文件夹重命名
         if isfile(source_file) and (i%step == 0):
             with open(target_file, 'wb') as tf:
                 with open(source_file, 'rb') as sf: 
@@ -73,7 +74,13 @@ def img_anno(anno_dir, img_dir, img_name, point_num = 8):
     '''
     调用图形接口鼠标点击进行标注
     '''
-    img = Image.open(img_dir + img_name +'.jpg')
+    anno_file = anno_dir + img_name + '.xml'
+    img_file = img_dir + img_name + '.jpg'
+    if exists(img_file) and exists(anno_file):#检测是否已经有标注
+        print(img_name + '.xml annotation already exist~')
+        return (0,0)
+    img = Image.open(img_file)
+    plt.title(img_file)
     plt.imshow(img)
     pos = plt.ginput(point_num)
     print(pos)
@@ -81,17 +88,29 @@ def img_anno(anno_dir, img_dir, img_name, point_num = 8):
     add_anno(anno_dir, img_dir, img_name, pos)
     return pos
 
-if __name__ == "__main__":
+def get_data(point_num, img_dir, anno_dir):
     print('begin to process~')
-    index = 10 
+    filenames = get_all_files(img_dir)
+    n = len(filenames)
+    for i in range(n):
+        img_anno(anno_dir, img_dir, filenames[i], point_num)
+    print('process over~')
+    
+if __name__ == "__main__":
+    index = 14
     step = 25
+    validation_step = 71
     point_num = 8
     pic_dir = '../data/rar/Section' + str(index) + 'CameraC/'
     img_dir = '../data/image/'
     anno_dir = '../data/annotation/'
-    select_pic(pic_dir, img_dir, step)
-    filenames = get_all_files(img_dir)
-    n = len(filenames)
-    for i in range(n):
-        pos = img_anno(anno_dir, img_dir, filenames[i])
-    print('process over~')
+    validation_img_dir = '../data/validation/image/'
+    validation_anno_dir = '../data/validation/annotation/'
+    #选择训练集图片
+    #select_pic(pic_dir, img_dir, step, index)
+    #选择验证集图片
+    #select_pic(pic_dir, validation_img_dir, validation_step, index)
+    #标注训练集
+    #get_data(point_num, img_dir, anno_dir)
+    #标注验证集
+    get_data(point_num, validation_img_dir, validation_anno_dir)
